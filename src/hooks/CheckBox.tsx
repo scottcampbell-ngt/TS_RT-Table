f2be1d55179b9dd578e268e9da6afc1bd58b9dbd
Scott <scott.campbell@numerated.com>
2021-08-13 16:41:54 -0400

    CheckBox check

--- src/hooks/CheckBox.tsx
+++ src/hooks/CheckBox.tsx
@@ -3,28 +3,13 @@ import { TableToggleAllRowsSelectedProps } from 'react-table';
 
 
 const Checkbox = React.forwardRef<HTMLInputElement, TableToggleAllRowsSelectedProps>(({ indeterminate, ...rest }, ref) => {
-	//const defaultRef = React.useRef<HTMLInputElement>(null);
 	
-
 	const defaultRef = React.useRef(null);
 	const resolvedRef = ref || defaultRef;
 
-	if ("current" in resolvedRef) console.log('hi');
-	
-	
-	//const resolvedRef = ref || defaultRef;
-	//if (typeof ref === 'function') return;
-
 	useEffect(() => {
 
-		if ("current" in resolvedRef) {
-		//	resolvedRef
-			//if ("indeterminate" in resolvedRef.current) {
-				if (typeof resolvedRef.current?.indeterminate === 'boolean') {
-					resolvedRef.current.indeterminate = !!indeterminate
-				//};
-			}
-		};
+		if ("current" in resolvedRef && typeof resolvedRef.current?.indeterminate === 'boolean') resolvedRef.current.indeterminate = !!indeterminate
 		
 	}, [ref, indeterminate]);
 
